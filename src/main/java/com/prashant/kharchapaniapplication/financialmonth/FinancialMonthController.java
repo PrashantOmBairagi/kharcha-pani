d65d0841e062b874e4cdefb3333bd91cb915e0b9
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-08-22 21:03:47 +0530

    major build: building FinancialMonth feature.

--- src/main/java/com/prashant/kharchapaniapplication/financialmonth/FinancialMonthController.java
+++ src/main/java/com/prashant/kharchapaniapplication/financialmonth/FinancialMonthController.java
@@ -1,13 +1,10 @@
 package com.prashant.kharchapaniapplication.financialmonth;
 
-import com.prashant.kharchapaniapplication.auth.AuthService;
-import com.prashant.kharchapaniapplication.user.User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.util.UUID;
 
 @RestController
@@ -16,19 +13,10 @@ import java.util.UUID;
 public class FinancialMonthController {
 
     private final FinancialMonthService fMonthService;
-    private final AuthService authService;
 
     @PostMapping()
     public ResponseEntity<FinancialMonth> createFMonth(@Valid @RequestBody FinancialMonthRequest request) {
-        User currentUser = authService.getCurrentUser();
-        FinancialMonth financialMonth = new FinancialMonth();
-        financialMonth.setUser(currentUser);
-        financialMonth.setBudget(request.getBudget());
-        financialMonth.setMonthlyIncome(request.getMonthlyIncome());
-        financialMonth.setMonth(request.getMonth());
-        financialMonth.setYear(request.getYear());
-        FinancialMonth saved = fMonthService.save(financialMonth);
-        return ResponseEntity.ok(saved);
+        return ResponseEntity.ok(fMonthService.createFMonth(request));
     }
 
     @PatchMapping("/{id}/budget")
