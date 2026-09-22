576f25679468ffdeae02dc9189826b8c1db2622e
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-09-22 19:28:37 +0530

    22-Sept-2026 Major Architecture change from Monolith to Microservices

---
+++ LenDen-services/src/test/java/com/KharchaPani/LenDenMicroservice/LenDenMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KharchaPani.LenDenMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenDenMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
