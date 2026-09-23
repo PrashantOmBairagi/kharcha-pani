60d6d2bd3474f5fc1934aff0ef6babebf6263bbc
PrashantOmBairagi <prashantbairagi2018@gmail.com>
2026-09-23 16:58:27 +0530

    build: 23-Sept-26 Building microservices of the Monolithic Kharcha Pani

--- LenDen-services/src/main/java/com/KharchaPani/LenDenMicroservice/controller/ClientController.java
+++ LenDen-services/src/main/java/com/KharchaPani/LenDenMicroservice/controller/ClientController.java
@@ -1,25 +1,40 @@
 package com.KharchaPani.LenDenMicroservice.controller;
 
 import com.KharchaPani.LenDenMicroservice.client.Client;
+import com.KharchaPani.LenDenMicroservice.client.ClientRequest;
 import com.KharchaPani.LenDenMicroservice.service.ClientService;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.UUID;
 
 @RequiredArgsConstructor
-@RestController("api/v2/lenden")
+@RestController("api/v2/lenden/client")
 public class ClientController {
 
     private final ClientService clientService;
 
     @PostMapping()
-    public ResponseEntity<Client> registerClient(Client requestedClient){
-        Client createdClient = clientService.createClient(requestedClient);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Client> registerClient(
+            @Valid @RequestBody ClientRequest request,
+            UUID userId
+        ){
+        Client createdClient = clientService.createClient(request,userId);
+        return ResponseEntity.ok().body(createdClient);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Client>> getAllClients(@RequestBody UUID userId){
+        List<Client> clients = clientService.getAllClients(userId);
+        return ResponseEntity.ok().body(clients);
+    }
+
+    @PatchMapping()
+    public ResponseEntity<Client> updateClient(@RequestBody String Name){
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
--- LenDen-services/src/main/java/com/KharchaPani/LenDenMicroservice/service/ClientService.java
+++ LenDen-services/src/main/java/com/KharchaPani/LenDenMicroservice/service/ClientService.java
@@ -1,13 +1,38 @@
 package com.KharchaPani.LenDenMicroservice.service;
 
 import com.KharchaPani.LenDenMicroservice.client.Client;
+import com.KharchaPani.LenDenMicroservice.client.ClientRequest;
+import com.KharchaPani.LenDenMicroservice.enums.ClientStatus;
+import com.KharchaPani.LenDenMicroservice.repository.ClientRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
 @Service
 public class ClientService {
 
-    public Client createClient(Client client){
-        Client newClient = new Client();
-        return client;
+    private final ClientRepository clientRepository;
+
+    public Client createClient(ClientRequest request, UUID userId){
+
+        Client client = new Client();
+        client.setUserID(userId);
+        client.setClientFirstName(request.getClientFirstName());
+        client.setClientLastName(request.getClientLastName());
+        client.setClientDescription(request.getClientDescription());
+        client.setClientMobileNumber(request.getClientMobileNumber());
+        client.setClientStatus(ClientStatus.ACTIVE);
+        client.setClientAlertsActive(Boolean.FALSE);
+        client.setNextSettlementDate(request.getNextSettlementDate());
+
+        return clientRepository.save(client);
     }
+
+    public List<Client> getAllClients(UUID userId){
+        return clientRepository.findAllByUserId(userId);
+    }
+
 }
